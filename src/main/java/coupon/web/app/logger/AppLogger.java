fc3487c7fdc0ca1f4a727c1b703fa85fca8054a8
krakkow <okowalsky@gmail.com>
2016-11-26 01:26:14 +0200

    logger

---
+++ src/main/java/coupon/web/app/logger/AppLogger.java
@@ -0,0 +1,18 @@
+package coupon.web.app.logger;
+
+import org.apache.log4j.Logger;
+
+
+/**
+ * Created by Kubal on 11/25/2016.
+ */
+public class AppLogger extends Logger
+    {
+        protected AppLogger(String name)
+            {
+                super(name);
+            }
+
+    }
+
+
